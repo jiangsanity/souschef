e8aa4d5a357cf00c38e0e9c051e7d315e15ea44a
Nick Worthington <nworthington16@gmail.com>
2020-02-23 18:33:39 -0500

    login and register screens

---
+++ app/components/HomeScreen.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
+} from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+	return (
+		<SafeAreaView style={homeStyles.container}>
+			<Text style={homeStyles.titleText}>
+                SousChef
+            </Text>
+            <TouchableOpacity 
+                style={homeStyles.button}
+                onPress={() => navigation.navigate("LoginScreen")} >
+                <Text style={homeStyles.txt}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                style={homeStyles.button} 
+                onPress={() => navigation.navigate("RegisterScreen")} >
+                <Text style={homeStyles.txt}>Register</Text>
+            </TouchableOpacity>
+		</SafeAreaView>
+	)
+}
+
+homeStyles = StyleSheet.create({
+	container: {
+		backgroundColor: '#6666aa',
+		width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    titleText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        fontFamily: 'times',
+        fontSize: 45,
+        marginTop: 200,
+        marginBottom: 180,
+        padding: 10,
+        textAlign: 'center'
+    },
+    txt: {
+        fontSize: 20,
+        color: 'black'
+    },
+    button: {
+        backgroundColor: '#ccc',
+        width: 200,
+        alignItems: 'center',
+        margin: 40,
+        padding: 10,
+        borderRadius: 5
+    }
+});
+
+export default HomeScreen;
---
+++ app/components/InputFieldAndLabel.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {
+	StyleSheet,
+    Text,
+    TextInput,
+    View
+} from 'react-native';
+
+const InputFieldAndLabel = ({ fieldName, fieldValue, setFieldValue, secure }) => {
+    return (
+        <View>
+            <Text style={registerStyles.txt}>{fieldName}</Text>
+            <TextInput 
+                placeholder={`${fieldName}...`} 
+                style={inputStyles.input} 
+                onChangeText={setFieldValue}
+                value={fieldValue}
+                autoCapitalize={"none"}
+                secureTextEntry={secure} />
+        </View>
+    )
+}
+
+inputStyles = StyleSheet.create({
+    input: {
+        height: 40,
+        width: 300,
+        padding: 10,
+        backgroundColor: 'white',
+        marginBottom: 40,
+    },
+
+});
+
+export default InputFieldAndLabel;
