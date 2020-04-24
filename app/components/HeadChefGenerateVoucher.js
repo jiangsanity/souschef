04b26a3e7bd2b2eb5d4bcf283b1191aed99eff84
James Jiang <jamesjiang@gatech.edu>
2020-04-23 22:15:47 -0400

    voucher files

---
+++ app/components/HeadChefGenerateVoucher.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TouchableOpacity, Button, Alert} from "react-native";
+
+const HeadChefGenerateVoucher = ({ route, navigation }) => {
+
+    return (
+        <View />
+    );
+}
+
+export default HeadChefGenerateVoucher;
